02b1c38b56dd6fee59cf067ecd2d02409c6d3c20
teampsych <42724812+teampsych@users.noreply.github.com>
2018-08-28 14:15:05 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,7 +12,7 @@ bot = commands.Bot(command_prefix="p.")
 @bot.event
 async def on_ready():
     print("Running")
-    await bot.change_presence(game=discord.Game(name='Pizza...'))
+    await bot.change_presence(game=discord.Game(name='Pizza... | v0.0'))
     for channel in member.server.channels:
         if channel.name == 'general' or channel.name == 'breakroom' or channel.name == 'announcements'
             await bot.send_message(channel, "Hi, guys! I'm just getting everything warmed up here, ehehe~!")
